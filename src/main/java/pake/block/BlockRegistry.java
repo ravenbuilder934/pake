be3cd4f4aa1fd90e08f77d9c12d2967f43c140c3
Raven <sabrinajamie@outlook.com>
2021-03-25 20:49:03 -0500

    fix hunger and sat values for pies

--- src/main/java/pake/block/BlockRegistry.java
+++ src/main/java/pake/block/BlockRegistry.java
@@ -2,7 +2,6 @@ package pake.block;
 
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
-import net.minecraft.block.CakeBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
 import net.minecraftforge.fml.RegistryObject;
@@ -14,7 +13,7 @@ public class BlockRegistry
 {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Pake.MOD_ID);
 
-    public static final RegistryObject<CakeBlock> sweetberrypie = BLOCKS.register("sweetberrypie", () -> new CakeBlock(AbstractBlock.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
+    public static final RegistryObject<PieBlock> sweetberrypie = BLOCKS.register("sweetberrypie", () -> new PieBlock(AbstractBlock.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
 
-    public static final RegistryObject<CakeBlock> applepie = BLOCKS.register("applepie", () -> new CakeBlock(AbstractBlock.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
+    public static final RegistryObject<PieBlock> applepie = BLOCKS.register("applepie", () -> new PieBlock(AbstractBlock.Properties.of(Material.CAKE).strength(0.5F).sound(SoundType.WOOL)));
 }
